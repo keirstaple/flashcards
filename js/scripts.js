c1e10a667b259fc2347d7dca9510c4ceb77b42b5
Andrew Telleria and Keir Staple <keirstaple@comcast.net>
2016-01-11 15:34:10 -0800

    create flashcards using javascript toggle function

---
+++ js/scripts.js
@@ -0,0 +1,92 @@
+$(document).ready(function() {
+  $(".word1").click(function() {
+    $("#flashcard1").toggle();
+    $("#definition1").toggle();
+  });
+
+  $(".word2").click(function() {
+    $("#flashcard2").toggle();
+    $("#definition2").toggle();
+  });
+
+  $(".word3").click(function() {
+    $("#flashcard3").toggle();
+    $("#definition3").toggle();
+  });
+
+  $(".word4").click(function() {
+    $("#flashcard4").toggle();
+    $("#definition4").toggle();
+  });
+
+  $(".word5").click(function() {
+    $("#flashcard5").toggle();
+    $("#definition5").toggle();
+  });
+
+  $(".word6").click(function() {
+    $("#flashcard6").toggle();
+    $("#definition6").toggle();
+  });
+
+  $(".word7").click(function() {
+    $("#flashcard7").toggle();
+    $("#definition7").toggle();
+  });
+
+  $(".word8").click(function() {
+    $("#flashcard8").toggle();
+    $("#definition8").toggle();
+  });
+
+  $(".word9").click(function() {
+    $("#flashcard9").toggle();
+    $("#definition9").toggle();
+  });
+
+  $(".word10").click(function() {
+    $("#flashcard10").toggle();
+    $("#definition10").toggle();
+  });
+
+  $(".word11").click(function() {
+    $("#flashcard11").toggle();
+    $("#definition11").toggle();
+  });
+
+  $(".word12").click(function() {
+    $("#flashcard12").toggle();
+    $("#definition12").toggle();
+  });
+
+  $(".word13").click(function() {
+    $("#flashcard13").toggle();
+    $("#definition13").toggle();
+  });
+
+  $(".word14").click(function() {
+    $("#flashcard14").toggle();
+    $("#definition14").toggle();
+  });
+
+  $(".word15").click(function() {
+    $("#flashcard15").toggle();
+    $("#definition15").toggle();
+  });
+
+  $(".word16").click(function() {
+    $("#flashcard16").toggle();
+    $("#definition16").toggle();
+  });
+
+  $(".word17").click(function() {
+    $("#flashcard17").toggle();
+    $("#definition17").toggle();
+  });
+
+  $(".word18").click(function() {
+    $("#flashcard18").toggle();
+    $("#definition18").toggle();
+  });
+
+});
